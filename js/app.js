5cbe3a14b3b2e4756c17588e942337d08f66aa59
Mingqin Tan <mttfd@mail.missouri.edu>
2015-10-23 14:29:09 -0500

    add search function

--- js/app.js
+++ js/app.js
@@ -14,12 +14,16 @@ var Excel = React.createClass({
     //for debugging purpose
     displayName: 'Excel',
 
+    //keep the originall data so that user can get back the whole table after canceling searching
+    _preSearchData: null,
+
     getInitialState: function() {
         return {
             data: this.props.initialData,
             sortBy: null,
             descending: false,
-            edit: null
+            edit: null,
+            search: false
         };
     },
 
@@ -53,26 +57,118 @@ var Excel = React.createClass({
     },
 
     _showEditor: function(e) {
-    	this.setState({edit: {
-    		row: parseInt(e.target.dataset.row, 10),
-    		cell: e.target.cellIndex
-    	}});
+        this.setState({
+            edit: {
+                row: parseInt(e.target.dataset.row, 10),
+                cell: e.target.cellIndex
+            }
+        });
     },
 
     _save: function(e) {
-		e.preventDefault();
-		var input = e.target.firstChild;
-    	var data = this.state.data.slice();
-    	data[this.state.edit.row][this.state.edit.cell] = input.value;
+        e.preventDefault();
+        var input = e.target.firstChild;
+        var data = this.state.data.slice();
+        data[this.state.edit.row][this.state.edit.cell] = input.value;
 
-    	this.setState({
-    		data: data,
-    		edit: null
-    	});
+        this.setState({
+            data: data,
+            edit: null
+        });
     },
 
-    render: function() {
-    	var self = this;
+    _toggleSearch: function() {
+        if (this.state.search === true) {
+            this.setState({
+                data: this._preSearchData,
+                search: false
+            });
+            this._preSearchData = null;
+        } else {
+            this._preSearchData = this.state.data;
+            this.setState({
+                search: true
+            });
+        }
+    },
+
+    _search: function(e) {
+        var needle = e.target.value.toLowerCase();
+        if (!needle) {
+            this.setState({
+                data: this._preSearchData
+            });
+            return;
+        }
+
+        var idx = e.target.dataset.idx;
+        var searchData = this._preSearchData.filter(function(row) {
+            return row[idx].toString().toLowerCase().indexOf(needle) > -1;
+        });
+        this.setState({
+            data: searchData
+        });
+    },
+
+
+    _download: function(format, e) {
+        var contents = format === 'json' ? JSON.stringify(this.state.data) : this.state.data.reduce(function(result, row) {
+        	return result + row.reduce(function(rowResult, cell, idx) {
+        		return rowResult += '"' + cell.replace(/"/g, '""') + '"' + (idx < row.length ? ',' : '');
+        	}, '') + '\n';
+        }, '');
+
+        var URL = window.URL || window.webkitURL;
+        var blob = new Blob([contents], {type: 'text/' + format});
+        e.target.href = URL.createObjectURL(blob);
+        e.target.download = 'data.' + format;
+    },
+
+    _renderToolbar: function() {
+        return React.DOM.div({
+                className: 'toolbar'
+            },
+            React.DOM.button({
+                onClick: this._toggleSearch,
+                className: 'toolbar'
+            }, 'search'),
+            React.DOM.a({
+                //感觉说白了bind就是一个函数currying的东西。。
+                onClick: this._download.bind(this, 'json'),
+                href: 'data.json'
+            }, 'Export JSON'),
+            React.DOM.a({
+                onClick: this._download.bind(this, 'csv'),
+                href: 'data.csv'
+            }, 'Export CSV')
+        );
+    },
+
+    _renderSearch: function() {
+        if (!this.state.search) {
+            return null;
+        }
+
+        return (
+            React.DOM.tr({
+                    onChange: this._search
+                },
+                this.props.headers.map(function(_ignore, idx) {
+                    return React.DOM.td({
+                            key: idx
+                        },
+                        React.DOM.input({
+                            type: 'text',
+                            'data-idx': idx
+                        })
+                    );
+                })
+            )
+        );
+    },
+
+    _renderTable: function() {
+        var self = this;
         return (
             React.DOM.table(null,
                 //delegates the event for tr..
@@ -82,32 +178,37 @@ var Excel = React.createClass({
                     React.DOM.tr(null,
                         //has to specify a unique key for every element in array
                         this.props.headers.map((function(title, idx) {
-                        	if(this.state.sortBy === idx) {
-                        		title += this.state.descending ? ' \u2191' : ' \u2193';
-                        	}
+                            if (this.state.sortBy === idx) {
+                                title += this.state.descending ? ' \u2191' : ' \u2193';
+                            }
                             return React.DOM.th({
                                 key: idx
                             }, title);
                         }).bind(this))
                     )
                 ),
-                React.DOM.tbody({onDoubleClick: self._showEditor},
+                React.DOM.tbody({
+                        onDoubleClick: self._showEditor
+                    },
+                    this._renderSearch(),
                     this.state.data.map(function(row, rowidx) {
                         return (
                             React.DOM.tr({
                                     key: rowidx
                                 },
                                 row.map(function(cell, idx) {
-                                	var content = cell;
-                                	var edit = self.state.edit;
-                                	if(edit && edit.row === rowidx && edit.cell === idx) {
-                                		var content = React.DOM.form({onSubmit: self._save},
-                                			React.DOM.input({
-                                				type: 'text',
-                                				defaultValue: content,
-                                			})
-                                		)
-                                	}
+                                    var content = cell;
+                                    var edit = self.state.edit;
+                                    if (edit && edit.row === rowidx && edit.cell === idx) {
+                                        content = React.DOM.form({
+                                                onSubmit: self._save
+                                            },
+                                            React.DOM.input({
+                                                type: 'text',
+                                                defaultValue: content
+                                            })
+                                        );
+                                    }
 
                                     return React.DOM.td({
                                         key: idx,
@@ -118,6 +219,15 @@ var Excel = React.createClass({
                         );
                     })
                 )
+            )
+        );
+    },
+
+    render: function() {
+        return (
+            React.DOM.div(null,
+                this._renderToolbar(),
+                this._renderTable()
             )
         );
     }
